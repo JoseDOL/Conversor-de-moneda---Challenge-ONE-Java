aa373a0a1d3bbf0fdd745d938e5d49a7f05c679a
JoseDOL <100498348+JoseDOL@users.noreply.github.com>
2023-02-26 15:03:11 -0600

    primero

---
+++ Conversor de moneda - Challenge ONE Java/src/conversor/de/moneda/challenge/one/java/ConversorDeMonedaChallengeONEJava.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conversor.de.moneda.challenge.one.java;
+
+import Ventanas.Principal;
+
+/**
+ *
+ * @author josed
+ */
+public class ConversorDeMonedaChallengeONEJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Principal vn = new Principal();
+    public static void main(String[] args) {
+        // TODO code application logic here
+        vn.show(true);
+        
+    }
+    
+}
